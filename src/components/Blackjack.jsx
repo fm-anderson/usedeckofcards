7cc10b1d0942de9b9594818f797fd43afedc3cc1
Anderson M <78233612+fm-anderson@users.noreply.github.com>
2024-05-04 17:42:49 -0400

    fix: blackjack image hidden on sm and md screen

--- src/components/Blackjack.jsx
+++ src/components/Blackjack.jsx
@@ -4,7 +4,7 @@ function Blackjack() {
       <img
         src="https://deckofcardsapi.com/static/img/blackjack2.png"
         alt="Blackjack"
-        className="absolute right-0 top-0 m-0 hidden h-auto w-52 md:flex"
+        className="absolute right-0 top-0 m-0 hidden h-auto w-52 lg:flex"
       />
     </a>
   );
