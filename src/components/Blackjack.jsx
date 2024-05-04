c9214cda5db70b16417c20401486170497de1d03
Anderson M <78233612+fm-anderson@users.noreply.github.com>
2024-05-04 17:36:53 -0400

    feature: link to deck of cards api

--- src/components/Blackjack.jsx
+++ src/components/Blackjack.jsx
@@ -1,10 +1,12 @@
 function Blackjack() {
   return (
-    <img
-      src="https://deckofcardsapi.com/static/img/blackjack2.png"
-      alt="Blackjack"
-      className="absolute right-0 top-0 m-0 hidden h-auto w-52 md:flex"
-    />
+    <a href="https://deckofcardsapi.com/" target="_blank">
+      <img
+        src="https://deckofcardsapi.com/static/img/blackjack2.png"
+        alt="Blackjack"
+        className="absolute right-0 top-0 m-0 hidden h-auto w-56 md:flex"
+      />
+    </a>
   );
 }
 
