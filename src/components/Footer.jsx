1090d812f1e12e1aeeb19f045849b9e54279f5e5
Anderson M <78233612+fm-anderson@users.noreply.github.com>
2024-05-06 23:43:23 -0400

    feature: npm icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,28 +21,59 @@ function Footer() {
         </svg>
         <p>Copyright © {new Date().getFullYear()} - useDeckOfCards</p>
       </div>
-      <div className="flex gap-4">
-        <a
-          href="https://github.com/fm-anderson/usedeckofcards"
-          target="_blank"
-          className="transition-transform duration-200 hover:scale-125"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="icon icon-tabler icon-tabler-brand-github"
-            width="32"
-            height="32"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="#ffffff"
-            fill="none"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
-          </svg>
-        </a>
+      <div>
+        <ul className="flex gap-4">
+          <li className="transition-transform duration-200 hover:scale-125">
+            <a
+              href="https://www.npmjs.com/package/usedeckofcards"
+              target="_blank"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-brand-npm"
+                width="32"
+                height="32"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="#ffffff"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M1 8h22v7h-12v2h-4v-2h-6z" />
+                <path d="M7 8v7" />
+                <path d="M14 8v7" />
+                <path d="M17 11v4" />
+                <path d="M4 11v4" />
+                <path d="M11 11v1" />
+                <path d="M20 11v4" />
+              </svg>
+            </a>
+          </li>
+          <li className="transition-transform duration-200 hover:scale-125">
+            <a
+              href="https://github.com/fm-anderson/usedeckofcards"
+              target="_blank"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-brand-github"
+                width="32"
+                height="32"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="#ffffff"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+              </svg>
+            </a>
+          </li>
+        </ul>
       </div>
     </footer>
   );
